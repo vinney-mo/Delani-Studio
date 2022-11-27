e741d3b033a0f6359949a92a391e348ab57d6bc1
vinney-mo <vinneymotanya@gmail.com>
2022-11-27 16:27:07 +0300

    Validations on contact form

--- js/scripts.js
+++ js/scripts.js
@@ -7,4 +7,26 @@ $(document).ready(function () {
     $(this).slideDown("1500").hide("1000");
     $(this).parent().find(".whatWeDoIcon").show("1500");
   });
+  $("#contactForm").submit(function (event) {
+    event.preventDefault();
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var message = $("#message").val();
+
+    let numberOfError = 0;
+    if (name == "") {
+      $("#nameError").show();
+      numberOfError++;
+    } else $("#nameError").hide();
+    if (email == "") {
+      $("#emailError").show();
+      numberOfError++;
+    } else $("#emailError").hide();
+    if (message == "") {
+      $("#messageError").show();
+      numberOfError++;
+    } else $("#messageError").hide();
+
+    if (numberOfError > 0) return false;
+  });
 });
